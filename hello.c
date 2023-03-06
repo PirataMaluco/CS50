f5b082dc01ff66401e1b78d2fbcc3262b13af3cc
PirataMaluco <123907718+PirataMaluco@users.noreply.github.com>
2023-03-06 22:50:19 +0000

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,3 +1,9 @@
+ /* ****************************
+  *  Week 01 - Problem Set 01  *
+  *          - Hello.c         *
+  *  Cláudio Dias - 25.02.2023 *
+  * ************************** */
+
 #include <cs50.h>
 #include <stdio.h>
 

ca0fe813fc2290437a2274820b6f2f2978f05dcc
PirataMaluco <123907718+PirataMaluco@users.noreply.github.com>
2023-03-06 22:57:01 +0000

    Update population.c

--- population.c
+++ population.c
@@ -1,8 +1,8 @@
  /* ***************************************************************************
-  *  Week 01 || Problem Set 01  || population.c ||  Cl·udio Dias - 25.02.2023 *
+  *  Week 01 || Problem Set 01  || population.c ||  Cl√°udio Dias - 25.02.2023 *
   * ************************************************************************* */
 
-  #include <cs50.h>
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
